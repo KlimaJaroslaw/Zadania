6d68af23563611db162567f3ff907d3a7961cd20
JaroslawKlima <jaroslaw.klima@aprosoft.pl>
2021-07-16 13:53:03 +0200

    04-Pracownik

---
+++ 04-Pracownik/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Pracownik
+{
+    class Employee
+    {
+        string imie1;
+        string nazwisko1;
+        double stawka1;
+        double wyplata1;
+        double doWyplaty;
+
+        public Employee(string imie, string nazwisko, double stawka)
+        {
+            this.imie1 = imie;
+            this.nazwisko1 = nazwisko;
+            this.stawka1 = stawka;
+        }
+
+        public void RegisterTime(double godziny)
+        {
+            if (godziny>8)
+            {
+                this.wyplata1 = 8 * stawka1 + (godziny - 8) * 2 * stawka1;
+            }
+            else
+            {
+                this.wyplata1 = godziny * stawka1;
+            }
+        }
+
+        public double DoWyplaty()
+        {            
+            doWyplaty = wyplata1;
+            wyplata1 = 0;
+            return doWyplaty;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine();
+            Console.WriteLine("imie- {0}", this.imie1);
+            Console.WriteLine("nazwisko- {0}", this.nazwisko1);
+            Console.WriteLine("wypłata- {0}",this.doWyplaty);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ 04-Pracownik/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Pracownik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char wyjscie;
+            do
+            {
+                string imie = "";
+                string nazwisko = "";
+                double stawka = 0;
+                double godziny = 0;
+
+
+                Console.Write("Podaj imię:");
+                imie = Console.ReadLine();
+
+                Console.Write("Podaj nazwisko:");
+                nazwisko = Console.ReadLine();
+
+                Liczba("Podaj stawkę: ", out stawka);
+                Liczba("Podaj godzdiny: ", out godziny);
+
+                Employee pracownik = new Employee(imie, nazwisko, stawka);
+                pracownik.RegisterTime(godziny);
+                pracownik.DoWyplaty();
+                pracownik.Info();
+
+                wyjscie = Console.ReadKey().KeyChar;
+            } while (!(wyjscie ==(char)ConsoleKey.Escape));
+                        
+        }
+
+        private static double Liczba(string a, out double l)
+        {
+            char znak;
+            string l_str = "";
+
+            Console.Write(a);
+            do
+            {
+                znak = Console.ReadKey(true).KeyChar;
+               
+                if (znak == (char)ConsoleKey.Enter && (l_str.Length) > 0)
+                    break;
+
+                if ("1234567890.,".Contains(znak))
+                {
+                    l_str += znak;
+                    Console.Write(znak);
+                }
+                else
+                {
+                    Console.Beep();
+                }
+            } while (true);
+
+            Console.WriteLine();
+            l = Convert.ToDouble(l_str);
+            return l;
+        }
+    }
+}
